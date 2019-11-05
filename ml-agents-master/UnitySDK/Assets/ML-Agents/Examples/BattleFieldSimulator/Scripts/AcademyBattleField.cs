1bfb20daac4261a3f602f7876f346de2db4f7631
Piotr <piotrblonski96@gmail.com>
2019-11-05 22:32:59 +0100

    spawner

--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/BattleFieldSimulator/Scripts/AcademyBattleField.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/BattleFieldSimulator/Scripts/AcademyBattleField.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AcademyBattleField : Academy
@@ -10,9 +11,19 @@ public class AcademyBattleField : Academy
     public static float[] rayAngles = { -90f, -45f, -135f, 0f, 45f, 90f, 135f, 180f, 110f, 70f };
     public Team team1 = new Team(Team.TeamTagEnum.Team1);
     public Team team2 = new Team(Team.TeamTagEnum.Team2);
+    public TeamSpawnPoint[] teamSpawnPoint;
     public override void InitializeAcademy()
     {
-       
+        teamSpawnPoint = FindObjectsOfType<TeamSpawnPoint>();
+        
+        foreach(var spawnPoint in teamSpawnPoint)
+        {
+            if (spawnPoint.tag == team1.TeamName)
+                spawnPoint.SpawnTeam(team1);
+            else if (spawnPoint.tag == team2.TeamName)
+                spawnPoint.SpawnTeam(team2);
+        }
+
     }
 
     public override void AcademyReset()
---
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/BattleFieldSimulator/Scripts/TeamSpawnPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamSpawnPoint : MonoBehaviour
+{
+    public GameObject team1Member;
+    public GameObject team2Member;
+    public int howManyWarriors = 0;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector3 startPos = transform.position;
+        Gizmos.DrawWireCube(startPos, new Vector3(howManyWarriors/4+1,1,howManyWarriors/4+1));
+    }
+
+    public void SpawnTeam(Team team)
+    {
+        Vector3 startPos = transform.position;
+        startPos.y = 0;
+        startPos.x -= 1;
+        startPos.z -= 1;
+        GameObject toInit;
+        if(team.TeamName==team1Member.tag)
+        {
+            toInit = team1Member;
+        }
+        else
+            toInit = team2Member;
+
+        for (int i = 1; i <= howManyWarriors; i++)
+        {
+            Instantiate(toInit,startPos,Quaternion.Euler(0,90,0));
+            startPos.z++;
+            if(i%4==0)
+            {
+                startPos.z -= 4;
+                startPos.x++;
+            }
+        }
+    }
+}
