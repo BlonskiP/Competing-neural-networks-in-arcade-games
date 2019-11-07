ab98e70c651deaa2e8fe604335eed14d112842f7
Piotr <piotrblonski96@gmail.com>
2019-11-07 22:15:48 +0100

    New ML-agents 0.11

---
+++ ml-agents 0.11/ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/BattleFieldSimulator/Scripts/AnimationEventListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventListener : MonoBehaviour
+{
+    ActionWarriorAgent agent;
+    private void Start()
+    {
+        agent = GetComponentInParent<ActionWarriorAgent>();
+    }
+    void DisableAttack()
+    {
+        agent.DisableAttack();
+    }
+    void EnableAttack()
+    {
+        agent.EnableAttack();
+    }
+}
